5f126a5c1069698b0a527092c375e7c06c8b670a
Fauzia599 <fauzali98@gmail.com>
2025-02-02 04:14:51 -0800

    my third commit

--- myproject/asgi.py
+++ myproject/asgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+setting_module='university.deployment'if 'RENDER_EXTEND':
+os.environ.setdefault('DJANGO _SETTINGS_MODULE',setting_module)
 
 application = get_asgi_application()
